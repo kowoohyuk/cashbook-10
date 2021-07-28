84762273bd00aec44f4f05fa018522cc2d2c5ca4
kowoohyuk <kowoohyuk91@gmail.com>
2021-07-28 15:55:39 +0900

    wip: :speech_balloon: 캘린더 클래스 작성
    - constructor 및 getter / setter 추가

--- client/src/utils/calendar.ts
+++ client/src/utils/calendar.ts
@@ -23,3 +23,47 @@ const ERROR = {
   target:
     'target 엘리먼트가 지정되어 있지않거나 HTMLElement가 아닙니다.\r\nsetTarget으로 렌더링이 될 target 엘리먼트를 지정해주세요.',
 };
+
+export default class Calendar {
+  private _date: Date;
+  private _$target: HTMLElement | null;
+  private _options: ICalendarOptions;
+  private _selected: Date | null;
+
+  constructor(
+    target = null,
+    date: Date = new Date(),
+    options = {
+      startWeek: 0,
+    },
+  ) {
+    this._date = date;
+    this._options = options;
+    this._$target = target;
+    this._selected = null;
+  }
+
+  get target(): HTMLElement | null {
+    return this._$target;
+  }
+
+  set target(target: HTMLElement | null) {
+    this._$target = target;
+  }
+
+  get date(): Date {
+    return this._date;
+  }
+
+  set date(date: Date) {
+    this._date = date;
+  }
+
+  get options(): ICalendarOptions {
+    return this._options;
+  }
+
+  set options(options: ICalendarOptions) {
+    this._options = options;
+  }
+}
