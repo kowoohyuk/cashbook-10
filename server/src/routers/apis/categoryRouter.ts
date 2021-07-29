fbb7e3339dd0c7d2c3132b83f9dab10376500cdc
kowoohyuk <kowoohyuk91@gmail.com>
2021-07-29 19:24:17 +0900

    wip: :speech_balloon: 카테고리 라우터 수정
    - 카테고리는최초에 설정된 카테고리만 사용하며, get 요청만 필요하므로 get을 제외한 모든 메소드를 삭제했습니다!

--- server/src/routers/apis/categoryRouter.ts
+++ server/src/routers/apis/categoryRouter.ts
@@ -1,25 +1,9 @@
 import { Router } from 'express';
+import Category from '../../models/category';
+import { getCategory } from '../../controllers/category.controller';
 
 const categoryRouter = Router();
 
-categoryRouter.get('/', (req, res) => {
-  console.log(req.method);
-  res.status(200).json({ message: '' });
-});
-
-categoryRouter.post('/', (req, res) => {
-  console.log(req.method);
-  res.status(200).json({ message: '' });
-});
-
-categoryRouter.put('/', (req, res) => {
-  console.log(req.method);
-  res.status(200).json({ message: '' });
-});
-
-categoryRouter.delete('/', (req, res) => {
-  console.log(req.method);
-  res.status(200).json({ message: '' });
-});
+categoryRouter.get('/', getCategory);
 
 export default categoryRouter;
