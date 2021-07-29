f1d3062910a4c0a9c3a0419cfc85651261a27b5d
Hov Lee <35447853+HobinLee@users.noreply.github.com>
2021-07-30 01:57:42 +0900

    feat: :sparkles: 히스토리 api 기능 구현

--- server/src/routers/apis/historyRouter.ts
+++ server/src/routers/apis/historyRouter.ts
@@ -1,25 +1,16 @@
-import { Router } from 'express';
+import express, { Router } from 'express';
+import {
+  deleteHistory,
+  putHistory,
+  postHistory,
+  getHistory,
+} from '../../controllers/history.controller';
 
 const historyRouter = Router();
 
-historyRouter.get('/', (req, res) => {
-  console.log(req.method);
-  res.status(200).json({ message: '' });
-});
-
-historyRouter.post('/', (req, res) => {
-  console.log(req.method);
-  res.status(200).json({ message: '' });
-});
-
-historyRouter.put('/', (req, res) => {
-  console.log(req.method);
-  res.status(200).json({ message: '' });
-});
-
-historyRouter.delete('/', (req, res) => {
-  console.log(req.method);
-  res.status(200).json({ message: '' });
-});
+historyRouter.get('/', getHistory);
+historyRouter.post('/', postHistory);
+historyRouter.put('/', putHistory);
+historyRouter.delete('/', deleteHistory);
 
 export default historyRouter;
