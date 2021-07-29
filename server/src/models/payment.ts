fa5311d84345bd5156f89e25bebb5618fa67dae5
kowoohyuk <kowoohyuk91@gmail.com>
2021-07-29 11:07:56 +0900

    wip: :speech_balloon: 페이먼트 모델 작성

---
+++ server/src/models/payment.ts
@@ -0,0 +1,30 @@
+import {
+  Table,
+  Column,
+  Model,
+  Unique,
+  AllowNull,
+  DataType,
+  HasMany,
+  PrimaryKey,
+} from 'sequelize-typescript';
+import PaymentList from './payment_list';
+
+export interface IPayment extends Document {
+  id?: number;
+  name: string;
+}
+
+@Table
+export default class Payment extends Model<IPayment> {
+  @PrimaryKey
+  @Column(DataType.INTEGER)
+  id: number;
+
+  @Unique
+  @Column(DataType.STRING)
+  name: string;
+
+  @HasMany(() => PaymentList)
+  paymentLists: PaymentList[];
+}
