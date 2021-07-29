fc35e9b93170fba914a98d9eef073256b89bc164
kowoohyuk <kowoohyuk91@gmail.com>
2021-07-29 11:10:15 +0900

    wip: :speech_balloon: 기본 포트 8000으로 변경

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,21 @@
-import express from "express";
-import path from "path";
+import express from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
+import { sequelize } from './models';
+import mainRouter from './routers';
+
+dotenv.config();
+sequelize.sync();
 const app = express();
 
-app.set("port", process.env.PORT || 3000);
-app.use("/", express.static(path.join(__dirname, "public")));
+app.set('port', process.env.PORT || 8000);
+// app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.use(express.static('dist'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(mainRouter);
 
-app.get("/", async (req, res) => {
-  console.log(req.method);
-});
-
-app.listen(app.get("port"), () => {
-  console.log(app.get("port"), "번 포트에서 대기중");
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기중');
 });
