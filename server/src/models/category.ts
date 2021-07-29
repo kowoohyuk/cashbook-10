d3b225a36e63483e6e07b3b73b77fd70a6339172
kowoohyuk <kowoohyuk91@gmail.com>
2021-07-29 11:16:55 +0900

    wip: :speech_balloon: 카테고리 모델 컬럼 추가

--- server/src/models/category.ts
+++ server/src/models/category.ts
@@ -1,27 +1,73 @@
-import { AllowNull, Column, Model, Table, Unique } from "sequelize-typescript";
+import {
+  AllowNull,
+  Column,
+  DataType,
+  HasMany,
+  Model,
+  Table,
+  Unique,
+} from 'sequelize-typescript';
+import History from './history';
 
 export const DEFAULT_CATEGORY = [
-  "생활",
-  "식비",
-  "교통",
-  "쇼핑/뷰티",
-  "의료/건강",
-  "문화/여가",
-  "미분류",
-  "월급",
-  "용돈",
-  "기타수입",
+  {
+    name: '생활',
+    isIncome: false,
+  },
+  {
+    name: '식비',
+    isIncome: false,
+  },
+  {
+    name: '교통',
+    isIncome: false,
+  },
+  {
+    name: '쇼핑/뷰티',
+    isIncome: false,
+  },
+  {
+    name: '의료/건강',
+    isIncome: false,
+  },
+  {
+    name: '문화/여가',
+    isIncome: false,
+  },
+  {
+    name: '미분류',
+    isIncome: false,
+  },
+  {
+    name: '월급',
+    isIncome: true,
+  },
+  {
+    name: '용돈',
+    isIncome: true,
+  },
+  {
+    name: '기타수입',
+    isIncome: true,
+  },
 ];
 
 export interface ICategory {
   id?: number;
   name: string;
+  isIncome: boolean;
 }
 
 @Table
 export default class Category extends Model<ICategory> {
   @AllowNull(false)
   @Unique
-  @Column
+  @Column(DataType.STRING)
   name: string;
+
+  @Column(DataType.BOOLEAN)
+  isIncome: boolean;
+
+  @HasMany(() => History)
+  histories: History[];
 }
