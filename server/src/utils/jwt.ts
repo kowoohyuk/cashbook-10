1ef5f51c6dffb381e48ff2a0314c5010ae3297fa
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-01 19:50:53 +0900

    wip: :speech_balloon: verifyToken 리턴 값 변경
    - 반환값에 에러 여부를 포함합니다.

--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -5,24 +5,22 @@ dotenv.config();
 
 const SECRET = process.env.SECRET || 'develop';
 
-const generateToken = (payload: string | object | Buffer) =>
+export const generateToken = (payload: string | object | Buffer) =>
   jwt.sign(payload, SECRET, {
     algorithm: 'HS256',
     expiresIn: '2h',
   });
 
-const verifyToken = (token: string) => {
+export const verifyToken = (token: string) => {
   try {
-    const decodedUser = jwt.verify(token, SECRET);
-    return decodedUser;
+    return {
+      error: false,
+      user: jwt.verify(token, SECRET).toString(),
+    };
   } catch (err) {
     return {
+      error: true,
       message: err.message,
     };
   }
-};
-
-export const token = {
-  generateToken,
-  verifyToken,
 };
