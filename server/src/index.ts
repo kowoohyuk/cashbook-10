c7ef6345727f9668aad24651ca1211025bb79bae
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-01 18:18:31 +0900

    wip: :speech_balloon: authRouter 수정
    - 모든 요청에 대해 인증 여부를 검사합니다.
    
    - jwt 타입 추가
    
    - history.controller
    util import 경로 변경

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,23 @@
 import express from 'express';
 import path from 'path';
+import morgan from 'morgan';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import { sequelize } from './models';
 import APIRouter from './routers/index';
+import authRouter from './routers/apis/authRouter';
 
 dotenv.config();
 sequelize.sync();
 const app = express();
 
 app.set('port', process.env.PORT || 8000);
-// app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.use(express.static('dist'));
 app.use(express.json());
+app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
 app.all('/*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
@@ -23,6 +27,9 @@ app.all('/*', function (req, res, next) {
   );
   next();
 });
+
+app.use(authRouter);
+
 app.use('/api/', APIRouter);
 
 app.listen(app.get('port'), () => {
