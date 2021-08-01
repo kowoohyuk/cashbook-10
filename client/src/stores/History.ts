5c2c4aedc1900aa4836cdea36b30dc7644b1ac2d
Hov Lee <35447853+HobinLee@users.noreply.github.com>
2021-08-01 22:13:59 +0900

    feat: :sparkles: 헤더기능 구현

--- client/src/stores/History.ts
+++ client/src/stores/History.ts
@@ -9,8 +9,12 @@ import { Store } from '../lib/woowact/core/Store';
 export const INIT_HISTORY = 'HISTORY/INIT' as const;
 export const DELETE_HISTORY = 'HISTORY/DELETE' as const;
 export const UPDATE_HISTORY = 'HISTORY/UPDATE' as const;
+export const PREV_MONTH = 'HISTORY/PREV_MONTH' as const;
+export const NEXT_MONTH = 'HISTORY/NEXT_MONTH' as const;
 
 type THistoryData = {
+  year: number;
+  month: number;
   histories: IHistory[];
 };
 
@@ -25,6 +29,8 @@ class HistoryStore extends Store<THistoryData> {
     [INIT_HISTORY]: this.initHistories.bind(this),
     [UPDATE_HISTORY]: this.updateHistories.bind(this),
     [DELETE_HISTORY]: this.deleteHistory.bind(this),
+    [PREV_MONTH]: this.prevMonth.bind(this),
+    [NEXT_MONTH]: this.nextMonth.bind(this),
   };
 
   findHistoryId = (historyId?: number): boolean => {
@@ -33,7 +39,7 @@ class HistoryStore extends Store<THistoryData> {
     );
   };
 
-  async deleteHistory(historyId?: number): Promise<THistoryData> {
+  async deleteHistory(historyId?: number): Promise<Partial<THistoryData>> {
     try {
       if (!historyId || !this.findHistoryId(historyId)) {
         throw new Error('History id가 없습니다');
@@ -56,7 +62,7 @@ class HistoryStore extends Store<THistoryData> {
     }
   }
 
-  async initHistories(): Promise<THistoryData> {
+  async initHistories(): Promise<Partial<THistoryData>> {
     const today = new Date();
 
     const histories = await getHistoryAPI({
@@ -67,26 +73,66 @@ class HistoryStore extends Store<THistoryData> {
     return { histories };
   }
 
-  async updateHistories(params: THistoryGetParams): Promise<THistoryData> {
+  async updateHistories(
+    params: THistoryGetParams,
+  ): Promise<Partial<THistoryData>> {
     const histories = await getHistoryAPI(params);
 
     return { histories };
   }
 
-  updateStore = (action: string, newHistory: Partial<THistoryData>) => {
-    if (!newHistory.histories) return;
+  async nextMonth() {
+    let month = this.data.month + 1;
+    let year = this.data.year;
 
-    const histories = newHistory.histories;
+    if (month > 12) {
+      month = 1;
+      year++;
+    }
+
+    const histories = await getHistoryAPI({
+      year,
+      month,
+    });
+
+    return { year, month, histories };
+  }
+
+  async prevMonth() {
+    let month = this.data.month - 1;
+    let year = this.data.year;
+
+    if (month < 1) {
+      month = 12;
+      year--;
+    }
+
+    const histories = await getHistoryAPI({
+      year,
+      month,
+    });
+
+    return { year, month, histories };
+  }
+
+  updateStore = (action: string, newHistory: Partial<THistoryData>) => {
+    if (!newHistory) return;
 
     switch (action) {
       case INIT_HISTORY:
-        this.updateData({ histories });
+        this.updateData({ ...newHistory });
         break;
       case UPDATE_HISTORY:
-        this.updateData({ histories });
+        this.updateData({ ...newHistory });
         break;
       case DELETE_HISTORY:
-        this.updateData({ histories });
+        this.updateData({ ...newHistory });
+        break;
+      case PREV_MONTH:
+        this.updateData({ ...newHistory });
+        break;
+      case NEXT_MONTH:
+        this.updateData({ ...newHistory });
         break;
       default:
         break;
@@ -94,4 +140,8 @@ class HistoryStore extends Store<THistoryData> {
   };
 }
 
-export const historyStore: HistoryStore = new HistoryStore({ histories: [] });
+export const historyStore: HistoryStore = new HistoryStore({
+  histories: [],
+  year: new Date().getFullYear(),
+  month: new Date().getMonth() + 1,
+});
