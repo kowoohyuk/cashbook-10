c5f258673e37ddb263ecfdab991f423d18c9157a
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-02 18:30:17 +0900

    wip: :speech_balloon: 캘린더 유틸 함수 추가
    - 현재 calendar에 포함되어 있습니다!
    
    - 변수명 오타 수정

--- client/src/utils/calendar.ts
+++ client/src/utils/calendar.ts
@@ -6,23 +6,46 @@ interface ICalendarOptions {
 type TDateObject = {
   year: number;
   month: number;
+  date?: number;
 };
 
-const convertDateToString = (date: Date) =>
-  `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+type TWeekTexts = {
+  [key: string]: string[];
+};
 
-const getStartWeek = (date: Date, copyDate = new Date(date)) =>
-  copyDate.setDate(1) && copyDate.getDay();
-
-const getLastDate = (date: Date, copyDate = new Date(date)) =>
-  copyDate.setDate(0) && copyDate.getDate();
-
-const WEEK_TEXTS = ['일', '월', '화', '수', '목', '금', '토', '일'];
+const WEEK_TEXTS: TWeekTexts = {
+  en: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+  ko: ['일', '월', '화', '수', '목', '금', '토'],
+};
 
 const ERROR = {
   target:
     'target 엘리먼트가 지정되어 있지않거나 HTMLElement가 아닙니다.\r\nsetTarget으로 렌더링이 될 target 엘리먼트를 지정해주세요.',
 };
+
+export const convertDateToString = (date: Date) =>
+  `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+
+export const getStartWeek = (date: Date, copyDate = new Date(date)) =>
+  copyDate.setDate(1) && copyDate.getDay();
+
+export const getLastDate = (date: Date, copyDate = new Date(date)) =>
+  copyDate.setDate(0) && copyDate.getDate();
+
+export const getDateOfWeek = (date: Date, language = 'ko') =>
+  WEEK_TEXTS[language][date.getDay()];
+
+export const getDateObject = (date: Date | string): TDateObject => {
+  date = new Date(date);
+  return {
+    year: date.getFullYear(),
+    month: date.getMonth() + 1,
+    date: date.getDate(),
+  };
+};
+
+export const checkValidDate = (date: Date | string) =>
+  !Number.isNaN(new Date(date).getTime());
 
 export default class Calendar {
   private _date: Date;
@@ -42,14 +65,14 @@ export default class Calendar {
     this._options = options;
     this._$target = target;
     this._selected = null;
-    this._dateObject = this.getDateObject();
+    this._dateObject = getDateObject(this._date);
   }
 
   render() {
     if (!this._$target) {
       return new Error(ERROR.target);
     }
-    this._$target.insertAdjacentHTML('afterend', this.getTemplate());
+    // return this.getTemplate();
   }
 
   rerender() {
@@ -57,11 +80,8 @@ export default class Calendar {
       return new Error(ERROR.target);
     }
     this._$target.querySelector('.woowa-calendar')?.remove();
-    this._$target.insertAdjacentHTML('afterend', this.getTemplate());
-  }
-
-  getTemplate(): string {
-    return 'return template';
+    // 리렌더링 관련한 함수 새로 작성
+    // this._$target.insertAdjacentHTML('afterend', );
   }
 
   getSelected(type: string = 'date'): Date | string | null {
@@ -71,17 +91,12 @@ export default class Calendar {
       : convertDateToString(this._selected);
   }
 
-  getDateObject(): TDateObject {
-    return {
-      year: this._date.getFullYear(),
-      month: this._date.getMonth() + 1,
-    };
-  }
-
   getMonth(): number {
     return this._dateObject.month;
   }
 
+  // nextBTN.addEventListener('click', this.setNextMonth);
+  // prevBTN.addEventListener('click', this.setNextMonth);
   setNextMonth(next = this._dateObject.month) {
     this._date.setMonth(next);
     this.rerender();
