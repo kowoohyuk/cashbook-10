820ca00172e067f4a6e1eb7a941e376cb53b0855
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-02 10:58:15 +0900

    wip: :speech_balloon: 유저 API 리팩토링
    - 비지니스 로직을 service으로 이관했습니다.
    
    - 유저 라우터의 잘못된 경로를 수정했습니다.
    
    - 스태이터스 코드를 어떻게 해야할지 고민이라 회의가 필요합니다!

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,51 +1,15 @@
-import { insertUser, selectUser } from '../services/user.service';
+import { signupUser, signinUser } from '../services/user.service';
 import { Request, Response } from 'express';
 import { HttpResponse, STATUS } from '.';
-import { generateToken } from '../utils/jwt';
-
-const MESSAGE = {
-  GET_FAIL: '유저 로그인 실패',
-  POST_FAIL: '유저 가입 실패',
-  POST_EXIST: '이미 존재하는 이메일입니다.',
-  POST_FAIL_VALIDATION: '유효하지 않은 이메일 입니다.',
-};
 
 export const postSignInUser = async (req: Request, res: Response) => {
-  try {
-    const { email, pw } = req.query;
-    const data = await selectUser(email as string, pw as string);
-    HttpResponse(res, STATUS.SUCCESS, {
-      data,
-    });
-  } catch (e) {
-    HttpResponse(res, STATUS.FAIL, {
-      message: MESSAGE.GET_FAIL,
-    });
-  }
+  const { email, pw } = req.body;
+  const data = await signinUser(email as string, pw as string);
+  HttpResponse(res, data.error ? STATUS.FAIL : STATUS.SUCCESS, data);
 };
 
 export const postSignUpUser = async (req: Request, res: Response) => {
-  try {
-    const { email, pw } = req.body;
-    const data = await insertUser(email as string, pw as string);
-    if (data.error) {
-      return HttpResponse(res, STATUS.SUCCESS, {
-        message: MESSAGE.POST_FAIL_VALIDATION,
-      });
-    }
-    const accessToken = generateToken({
-      email,
-      pw,
-    });
-    HttpResponse(res, STATUS.SUCCESS, {
-      data: {
-        email,
-        accessToken,
-      },
-    });
-  } catch (e) {
-    HttpResponse(res, STATUS.FAIL, {
-      message: MESSAGE.POST_FAIL,
-    });
-  }
+  const { email, pw } = req.body;
+  const data = await signupUser(email as string, pw as string);
+  HttpResponse(res, data.error ? STATUS.FAIL : STATUS.SUCCESS, data);
 };
