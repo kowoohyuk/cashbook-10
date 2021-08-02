822d79b317f42be79fbbb34d7b31ed4092e5c433
Hov Lee <35447853+HobinLee@users.noreply.github.com>
2021-08-02 16:45:57 +0900

    fix: api 요청 withCredentials 에서 credentials로 수정

--- client/src/apis/api.ts
+++ client/src/apis/api.ts
@@ -20,7 +20,7 @@ const useFetch = async (url: string, method: Method, body?: {}) => {
     method: method,
     headers: {
       'Content-Type': 'application/json',
-      withCredentials: 'true',
+      credentials: 'include',
     },
     body: body ? JSON.stringify(body) : undefined,
   };
