fe5c41a464e61a3b8a59efeae5d9a80a3043a25f
Hov Lee <35447853+HobinLee@users.noreply.github.com>
2021-08-02 11:16:15 +0900

    chore: :guitar: 이미지 파일 추가

---
+++ client/src/useResource.ts
@@ -0,0 +1,23 @@
+import addSVG from './assets/add.svg';
+import checkSVG from './assets/check.svg';
+import leftArrowSVG from './assets/chevron-left.svg';
+import rightArrowSVG from './assets/chevron-right.svg';
+import closeWhiteSVG from './assets/close-white.svg';
+import moreDefaultSVG from './assets/more-vertical.svg';
+import userSVG from './assets/user.svg';
+import deleteSVG from './assets/delete.svg';
+import editSVG from './assets/edit.svg';
+import emptyPNG from './assets/emptyList.png';
+
+export {
+  userSVG,
+  checkSVG,
+  leftArrowSVG,
+  moreDefaultSVG,
+  rightArrowSVG,
+  closeWhiteSVG,
+  addSVG,
+  deleteSVG,
+  editSVG,
+  emptyPNG,
+};
