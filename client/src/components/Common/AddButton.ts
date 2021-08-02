23627ad0d1740a695094ec7d58befdac60db2d9b
Hov Lee <35447853+HobinLee@users.noreply.github.com>
2021-08-02 19:11:56 +0900

    test: 모달 기능 기본 테스트를 위한 스타일링 및 구현

--- client/src/components/Common/AddButton.ts
+++ client/src/components/Common/AddButton.ts
@@ -1,7 +1,9 @@
 import IMGButton from './IMGButton';
 import { addSVG } from '../../useResource';
+import { modalStore, OPEN_MODAL } from '../../stores/Modal';
 
 export default class AddButton extends IMGButton {
+  $el: HTMLElement;
   constructor() {
     super({
       src: addSVG,
@@ -9,8 +11,13 @@ export default class AddButton extends IMGButton {
       onclick: () => this.onclick(),
     });
 
+    this.$el = document.createElement('div');
+    this.$el.innerHTML = '<h2>안녕하세요</h2>';
+
     this.init();
   }
 
-  onclick() {}
+  onclick() {
+    modalStore.dispatch(OPEN_MODAL, this.$el);
+  }
 }
