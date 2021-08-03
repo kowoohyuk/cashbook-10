70b9469badf56c71f10bc1a5026a17ba7678f004
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-04 02:28:57 +0900

    wip: :speech_balloon: 차트 데이터 생성 함수 작성
    - 테스트용 데이터를 추가했습니다.

--- client/src/components/Chart/ChartSection.ts
+++ client/src/components/Chart/ChartSection.ts
@@ -12,11 +12,103 @@ type TDonutArcData = {
   isLargeArcFlag: number;
 };
 
+type TChartData = {
+  id: number;
+  name: string;
+  color: string;
+  amount: number;
+  percent: number;
+};
+
+// 임시
+const histories: any[] = [
+  {
+    amount: 150000,
+    categoryId: 1,
+    isIncome: false,
+    name: '1번',
+  },
+  {
+    amount: 150000,
+    categoryId: 2,
+    isIncome: false,
+    name: '2번',
+  },
+  {
+    amount: 150000,
+    categoryId: 3,
+    isIncome: false,
+    name: '3번',
+  },
+  {
+    amount: 150000,
+    categoryId: 5,
+    isIncome: false,
+    name: '5번',
+  },
+  {
+    amount: 177000,
+    categoryId: 6,
+    isIncome: false,
+    name: '6번',
+  },
+  {
+    amount: 10000,
+    categoryId: 4,
+    isIncome: false,
+    name: '7번',
+  },
+  {
+    amount: 15000,
+    categoryId: 3,
+    isIncome: false,
+    name: '9번',
+  },
+  {
+    amount: 33000,
+    categoryId: 2,
+    isIncome: false,
+    name: '3번',
+  },
+  {
+    amount: 8000,
+    categoryId: 1,
+    isIncome: false,
+    name: '2번',
+  },
+];
+
 export default class ChartSection extends Component {
   constructor() {
     super({});
     historyStore.subscribe(this);
     this.init();
+  }
+
+  createChartData(histories: any[]) {
+    let sum = 0;
+    const data: TChartData[] = histories.reduce((acc, cur) => {
+      if (acc[cur.categoryId]) {
+        acc[cur.categoryId].amount += cur.amount;
+      } else {
+        const { name, color } = CATERORIES[cur.categoryId];
+        acc[cur.categoryId] = {
+          id: cur.categoryId,
+          name,
+          color,
+          percent: 0,
+          amount: cur.amount,
+        };
+      }
+      sum += cur.amount;
+      return acc;
+    }, []);
+    data.sort((a, b) => b.amount - a.amount);
+    data.forEach(d => {
+      d.percent = d.amount / sum;
+    });
+    console.log(data);
+    return data;
   }
 
   render() {
