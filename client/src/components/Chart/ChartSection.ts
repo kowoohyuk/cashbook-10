cad3bc6b7b1734f317cf6c6e4c0e879eec21dcad
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-04 02:27:19 +0900

    wip: :speech_balloon: 도넛차트 타입 작성

--- client/src/components/Chart/ChartSection.ts
+++ client/src/components/Chart/ChartSection.ts
@@ -1,13 +1,32 @@
+import { IHistory } from '../../apis/historyAPI';
+import { CATERORIES } from '../../utils/categories';
 import { Component } from '../../lib/woowact/index';
+import { historyStore } from '../../stores/History';
+import '../../styles/chart/chartSection';
+
+type TDonutArcData = {
+  startX: number;
+  startY: number;
+  endX: number;
+  endY: number;
+  isLargeArcFlag: number;
+};
 
 export default class ChartSection extends Component {
   constructor() {
     super({});
+    historyStore.subscribe(this);
     this.init();
   }
+
   render() {
     return `
     <section class="chart-section">
+    <div class="svg-wrapper">
+      <svg width="250" height="250" viewBox="-1.5 -1.5 3 3">
+        ${this.generateChart()}
+      </svg>
+    </div>
     </section>
     `;
   }
