31141b6191f18eacd6dac50e8d7c0d300c5c3950
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-03 11:33:30 +0900

    wip: :speech_balloon: 히스토리 이용, 캘린더 컨텐츠 생성 함수 추가

--- client/src/components/Calendar/CalendarSection.ts
+++ client/src/components/Calendar/CalendarSection.ts
@@ -1,25 +1,44 @@
 import { Component } from '../../lib/woowact/index';
 import { historyStore } from '../../stores/History';
-import Calendar from '../../utils/calendar/calendar';
+import Calendar, { getLastDate } from '../../utils/calendar/calendar';
 import { $ } from '../../utils/selector';
 import '../../styles/calendar/calendarSection';
+import { IHistory } from '../../apis/historyAPI';
 
 export default class CalendarSection extends Component {
-  // $calendar: Component;
-  calender: Calendar;
+  calendar: Calendar;
 
   constructor() {
     super({});
-    this.calender = new Calendar();
+    this.calendar = new Calendar();
     historyStore.subscribe(this);
     this.init();
   }
 
   componentDidUpdate() {
-    this.calender.target = this.$element;
-    this.calender.setDate(
-      `${historyStore.data.year}-${historyStore.data.month}`,
+    this.calendar.contents = this.createContents(historyStore.data.histories);
+    this.calendar.target = this.$element;
+    this.calendar.date = `${historyStore.data.year}-${historyStore.data.month}`;
+  }
+
+  createContents(histories: IHistory[]) {
+    const contents = Array.from(
+      Array(
+        getLastDate(
+          new Date(`${historyStore.data.year}-${historyStore.data.month}`),
+        ),
+      ),
+      () => new Array(),
     );
+    histories.forEach(({ amount, paymentDate, isIncome }) =>
+      contents[new Date(paymentDate).getDate() - 1].push(
+        `<div class="calendar-history ${isIncome ? 'income' : ''}">${
+          (isIncome ? 1 : -1) * amount
+        }</div>`,
+      ),
+    );
+    // );
+    return contents.map(content => content.join(''));
   }
 
   render() {
