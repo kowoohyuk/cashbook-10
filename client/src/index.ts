3de954fbf374ccde8c156151ea92de749ba8e208
Hov Lee <35447853+HobinLee@users.noreply.github.com>
2021-08-06 11:56:24 +0900

    feat: :sparkles: 토큰 url로 가져오

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,3 +1,7 @@
 import App from './App';
 
+const token = new URL(location.href).searchParams.get('token');
+token && localStorage.setItem('token', token);
+history.replaceState({}, '', '/');
+
 new App();
